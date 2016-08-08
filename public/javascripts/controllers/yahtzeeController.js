ddd799253bb51179a76b7c72b166a12f3e97ad10
william-mcdermott <williammcdermott@Williams-MacBook-Pro.local>
2016-08-08 15:57:40 -0500

    add route provider

--- public/javascripts/controllers/yahtzeeController.js
+++ public/javascripts/controllers/yahtzeeController.js
@@ -1,5 +1,13 @@
 var app = angular.module('yahtzeeApp', ['ngRoute']);
 
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('gameboard', {
+      templateUrl: '../../views/gameboard.html',
+      controller: 'yahtzeeCtrl'
+    });
+});
+
 app.controller('yahtzeeCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray){
   var ref = new Firebase('https://billandaaronsyahtzee.firebaseio.com/');
 }]);
