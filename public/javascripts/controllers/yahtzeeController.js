77d92cec4ca9b212647cfb0d86d8d722a66e6093
william-mcdermott <williammcdermott@Williams-MacBook-Pro.local>
2016-08-09 17:06:21 -0500

    3-way data binding again

--- public/javascripts/controllers/yahtzeeController.js
+++ public/javascripts/controllers/yahtzeeController.js
@@ -21,54 +21,56 @@ app.config( function( $routeProvider, $httpProvider, $locationProvider ) {
 	} )
 } );
 app.controller( 'yahtzeeCtrl', [ '$scope', '$firebaseArray', '$firebaseObject', function( $scope, $firebaseArray, $firebaseObject ) {
+	var ref = firebase.database().ref().child("player1");
+	$firebaseObject(ref).$bindTo($scope, "view.player1");
 	$scope.view = {};
 	$scope.view.player1 = {};
 	$scope.view.player2 = {};
-	$scope.view.player1.aces = 0;
-	$scope.view.player1.twos = 0;
-	$scope.view.player1.threes = 0;
-	$scope.view.player1.fours = 0;
-	$scope.view.player1.fives = 0;
-	$scope.view.player1.sixes = 0;
-	$scope.view.player1.uppersubtotal = $scope.view.player1.aces + $scope.view.player1.twos + $scope.view.player1.threes + $scope.view.player1.fours + $scope.view.player1.fives + $scope.view.player1.sixes;
-	$scope.view.player1.uppertotal = 0;
-	$scope.view.player1.K3 = 0;
-	$scope.view.player1.K4 = 0;
-	$scope.view.player1.FH = 0;
-	$scope.view.player1.SmS = 0;
-	$scope.view.player1.LgS = 0;
-	$scope.view.player1.yahtzee = 0;
-	$scope.view.player1.chance = 0;
-	$scope.view.player1.yahtzeebonus = 0;
-	$scope.view.player1.lowertotal = $scope.view.player1.K3 + $scope.view.player1.K4 + $scope.view.player1.FH + $scope.view.player1.SmS + $scope.view.player1.LgS + $scope.view.player1.yahtzee + $scope.view.player1.chance + $scope.view.player1.yahtzeebonus;
-	$scope.view.player1.grandtotal = $scope.view.player1.uppertotal + $scope.view.player1.lowertotal;
-	$scope.view.player2.aces = 0;
-	$scope.view.player2.twos = 0;
-	$scope.view.player2.threes = 0;
-	$scope.view.player2.fours = 0;
-	$scope.view.player2.fives = 0;
-	$scope.view.player2.sixes = 0;
-	$scope.view.player2.uppersubtotal = $scope.view.player2.aces + $scope.view.player2.twos + $scope.view.player2.threes + $scope.view.player2.fours + $scope.view.player2.fives + $scope.view.player2.sixes;
-	$scope.view.player2.uppertotal = 0;
-	$scope.view.player2.K3 = 0;
-	$scope.view.player2.K4 = 0;
-	$scope.view.player2.FH = 0;
-	$scope.view.player2.SmS = 0;
-	$scope.view.player2.LgS = 0;
-	$scope.view.player2.yahtzee = 0;
-	$scope.view.player2.chance = 0;
-	$scope.view.player2.yahtzeebonus = 0;
-	$scope.view.player2.lowertotal = $scope.view.player2.K3 + $scope.view.player2.K4 + $scope.view.player2.FH + $scope.view.player2.SmS + $scope.view.player2.LgS + $scope.view.player2.yahtzee + $scope.view.player2.chance + $scope.view.player2.yahtzeebonus;
-	$scope.view.player2.grandtotal = $scope.view.player2.uppertotal + $scope.view.player2.lowertotal;
-
-	var ref = firebase.database().ref().child("player1");
-	$firebaseObject(ref).$bindTo($scope, "view.player1");
-	$scope.playerCard = $firebaseObject(ref);
-	$scope.view.addOnes = function(){
-		$scope.view.$save({
-			aces: $scope.view.player1.aces
-		});
-	};
+	// $scope.view.player1.aces = 0;
+	// $scope.view.player1.twos = 0;
+	// $scope.view.player1.threes = 0;
+	// $scope.view.player1.fours = 0;
+	// $scope.view.player1.fives = 0;
+	// $scope.view.player1.sixes = 0;
+	// $scope.view.player1.uppersubtotal = 0;
+	// $scope.view.player1.uppertotal = 0;
+	// $scope.view.player1.K3 = 0;
+	// $scope.view.player1.K4 = 0;
+	// $scope.view.player1.FH = 0;
+	// $scope.view.player1.SmS = 0;
+	// $scope.view.player1.LgS = 0;
+	// $scope.view.player1.yahtzee = 0;
+	// $scope.view.player1.chance = 0;
+	// $scope.view.player1.yahtzeebonus = 0;
+	// $scope.view.player1.lowertotal = $scope.view.player1.K3 + $scope.view.player1.K4 + $scope.view.player1.FH + $scope.view.player1.SmS + $scope.view.player1.LgS + $scope.view.player1.yahtzee + $scope.view.player1.chance + $scope.view.player1.yahtzeebonus;
+	// $scope.view.player1.grandtotal = $scope.view.player1.uppertotal + $scope.view.player1.lowertotal;
+	// $scope.view.player2.aces = 0;
+	// $scope.view.player2.twos = 0;
+	// $scope.view.player2.threes = 0;
+	// $scope.view.player2.fours = 0;
+	// $scope.view.player2.fives = 0;
+	// $scope.view.player2.sixes = 0;
+	// $scope.view.player2.uppersubtotal = $scope.view.player2.aces + $scope.view.player2.twos + $scope.view.player2.threes + $scope.view.player2.fours + $scope.view.player2.fives + $scope.view.player2.sixes;
+	// $scope.view.player2.uppertotal = 0;
+	// $scope.view.player2.K3 = 0;
+	// $scope.view.player2.K4 = 0;
+	// $scope.view.player2.FH = 0;
+	// $scope.view.player2.SmS = 0;
+	// $scope.view.player2.LgS = 0;
+	// $scope.view.player2.yahtzee = 0;
+	// $scope.view.player2.chance = 0;
+	// $scope.view.player2.yahtzeebonus = 0;
+	// $scope.view.player2.lowertotal = $scope.view.player2.K3 + $scope.view.player2.K4 + $scope.view.player2.FH + $scope.view.player2.SmS + $scope.view.player2.LgS + $scope.view.player2.yahtzee + $scope.view.player2.chance + $scope.view.player2.yahtzeebonus;
+	// $scope.view.player2.grandtotal = $scope.view.player2.uppertotal + $scope.view.player2.lowertotal;
+	$scope.updateFirstPlayerUpperScore = function(scoreToUpdate){
+		console.log("Called");
+		console.log($scope.view.player1[scoreToUpdate]);
+		$scope.view.player1[scoreToUpdate]+=1;
+		$scope.view.player1.uppersubtotal=parseInt($scope.view.player1.aces + $scope.view.player1.twos + $scope.view.player1.threes + $scope.view.player1.fours + $scope.view.player1.fives + $scope.view.player1.sixes);
+		console.log($scope.view.player1.uppersubtotal);
+		console.log($scope.view.player1.twos);
+	}
+//	$scope.playerCard = $firebaseObject(ref);
 } ] );
 
 app.controller( 'IndexController', [ '$scope', '$firebaseArray', function( $scope, $firebaseArray ) {} ] );
