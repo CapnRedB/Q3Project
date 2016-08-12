bc4e5856965afb2f392d4c9cebe46d4d4ade1541
Aaron Levinson <aaron.levinson1000@gmail.com>
2016-08-12 10:49:54 -0500

    Check check

--- public/javascripts/controllers/yahtzeeController.js
+++ public/javascripts/controllers/yahtzeeController.js
@@ -38,6 +38,71 @@ app.factory( "Auth", [ "$firebaseAuth",
 
 app.controller( "signUpCtrl", [ "$scope", "Auth",
   function( $scope, Auth ) {
+		$scope.toggleSignIn = function() {
+			console.log( "SIGN IT" );
+			if ( firebase.auth().currentUser ) {
+				// [START signout]
+				firebase.auth().signOut();
+				// [END signout]
+			} else {
+				var email = document.getElementById( 'email' ).value;
+				var password = document.getElementById( 'password' ).value;
+				if ( email.length < 4 ) {
+					alert( 'Please enter an email address.' );
+					return;
+				}
+				if ( password.length < 4 ) {
+					alert( 'Please enter a password.' );
+					return;
+				}
+				// Sign in with email and pass.
+				// [START authwithemail]
+				firebase.auth().signInWithEmailAndPassword( email, password ).catch( function( error ) {
+					// Handle Errors here.
+					var errorCode = error.code;
+					var errorMessage = error.message;
+					// [START_EXCLUDE]
+					if ( errorCode === 'auth/wrong-password' ) {
+						alert( 'Wrong password.' );
+					} else {
+						alert( errorMessage );
+					}
+					console.log( error );
+					document.getElementById( 'quickstart-sign-in' ).disabled = false;
+					// [END_EXCLUDE]
+				} );
+				// [END authwithemail]
+			}
+			document.getElementById( 'quickstart-sign-in' ).disabled = true;
+		}
+		$scope.handleSignUp = function() {
+			var email = document.getElementById( 'email' ).value;
+			var password = document.getElementById( 'password' ).value;
+			if ( email.length < 4 ) {
+				alert( 'Please enter an email address.' );
+				return;
+			}
+			if ( password.length < 4 ) {
+				alert( 'Please enter a password.' );
+				return;
+			}
+			// Sign in with email and pass.
+			// [START createwithemail]
+			firebase.auth().createUserWithEmailAndPassword( email, password ).catch( function( error ) {
+				// Handle Errors here.
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				// [START_EXCLUDE]
+				if ( errorCode == 'auth/weak-password' ) {
+					alert( 'The password is too weak.' );
+				} else {
+					alert( errorMessage );
+				}
+				console.log( error );
+				// [END_EXCLUDE]
+			} );
+			// [END createwithemail]
+		}
 		$scope.auth = Auth;
 
 		// any time auth state changes, add the user data to scope
